eae240a191d0ae332e19b41baad40c27c09ed626
AbusedAgain <llcsurgeservices@gmail.com>
2026-08-17 11:40:46 -0700

    loader

---
+++ zS1Tv5D/UIEngine/framework/gui.cpp
@@ -0,0 +1,159 @@
+﻿#include "headers/includes.h"
+#include "headers/widgets.h"
+
+void c_gui::render()
+{
+	gui->initialize();
+
+	gui->push_var(style_var_window_shadow_size, SCALE(0, 0));
+	gui->set_next_window_pos(ImVec2(0, 0));
+	gui->set_next_window_size(SCALE(var->window.size));
+	gui->begin("menu", nullptr, var->window.flags);
+	{
+		gui->set_style();
+		gui->draw_decorations();
+
+		gui->easing(var->gui.stage_alpha, var->gui.active_stage == var->gui.stage_count ? 1.f : 0.f, 6.f, static_easing);
+		if (var->gui.stage_alpha == 0.f)
+			var->gui.active_stage = var->gui.stage_count;
+
+		gui->easing(var->gui.content_alpha, var->gui.active_section == var->gui.section_count ? 1.f : 0.f, 6.f, static_easing);
+
+		if (var->gui.content_alpha == 0.f)
+			var->gui.active_section = var->gui.section_count;
+
+		gui->easing(var->window.size.x, var->gui.stage_count > 0 ? var->window.new_width : var->window.default_size.x, 800.f, static_easing);
+		gui->easing(var->window.size.y, var->gui.stage_count > 1 ? (var->gui.section_count == 1 ? var->window.section_1_height : var->window.stage_2_height) : (var->gui.section_count == 1 ? var->window.section_1_height : var->window.default_size.y), 200.f, static_easing);
+
+		gui->set_pos(SCALE(0, 0), pos_all);
+		gui->begin_content("content", gui->window_size(), SCALE(elements->window.padding), SCALE(elements->widgets.spacing.x, elements->window.padding.y), window_flags_no_scroll_with_mouse | window_flags_no_scrollbar);
+		{
+			gui->push_var(style_var_alpha, var->gui.stage_alpha);
+			if (var->gui.active_stage > 0)
+			{
+				widgets->top_bar("t.me/uiengine", "22.08.2025");
+				gui->sameline();
+				widgets->settings_button();
+			}
+			gui->pop_var();
+			gui->push_var(style_var_alpha, var->gui.content_alpha * var->gui.stage_alpha);
+			if (var->gui.active_section == 1)
+			{
+				widgets->widgets_child("General", gui->language("General", "Общие", true), "O");
+				{
+					static bool launch_win{ true };
+					widgets->checkbox_ex("Launch on Windows startup", gui->language("Launch on Windows startup", "Запуск при старте Windows", true), &launch_win);
+					static bool start_min{ false };
+					widgets->checkbox_ex("Start minimized to tray", gui->language("Start minimized to tray", "Запуск сворачивается в трей", true), &start_min);
+					static bool show_up{ true };
+					widgets->checkbox_ex("Show Update Notifications", gui->language("Show Update Notifications", "Показать уведомления об обновлениях", true), &show_up);
+				}
+				widgets->widgets_end_child();
+				gui->set_screen_pos(GImGui->LastItemData.Rect.Max.y + SCALE(elements->widgets.spacing.y), pos_y);
+				widgets->selection_buttons("Language", gui->language("Language", "Язык", true), "English", "Русский", var->gui.lang_count);
+
+				widgets->widgets_child("Integration", gui->language("Integration", "Интеграция", true), "P");
+				{
+					static bool auto_in{ false };
+					widgets->checkbox_ex("Auto-Inject on Game Launch", gui->language("Auto-Inject on Game Launch", "Автоинъекция при запуске игры", true), &auto_in);
+					static bool enable_in{ true };
+					widgets->checkbox_ex("Enable In-Game Overlay", gui->language("Enable In-Game Overlay", "Включение игрового наложения", true), &enable_in);
+				}
+				widgets->widgets_end_child();
+				static int process_sel{ 1 };
+				gui->set_screen_pos(GImGui->LastItemData.Rect.Max.y + SCALE(elements->widgets.spacing.y), pos_y);
+				widgets->selection_buttons("Game Process Priority", gui->language("Game Process Priority", "Приоритет игрового процесса", true), gui->language("Normal", "Нормальный", true), gui->language("High", "Высокий", true), process_sel);
+
+				widgets->widgets_child("Security", gui->language("Security", "Безопасность", true), "Q");
+				{
+					static bool auto_cl{ true };
+					widgets->checkbox_ex("Automatically Clear Traces on Exit", gui->language("Automatically Clear Traces on Exit", "Автоматическая очистка следов при выходе", true), &auto_cl);
+					static bool enable_hwid{ true };
+					widgets->checkbox_ex("Enable HWID Spoofer", gui->language("Enable HWID Spoofer", "Включить HWID Spoofer", true), &enable_hwid);
+					static bool self_d{ false };
+					widgets->checkbox_ex("Self-Destruct on Analysis Detection", gui->language("Self-Destruct on Analysis Detection", "Самоуничтожение при обнаружении анализа", true), &self_d);
+				}
+				widgets->widgets_end_child();
+			}
+
+			if(var->gui.active_section == 0)
+			{
+				if (var->gui.active_stage == 0)
+					widgets->log_reg_page();
+				gui->begin_content("s_g_content", SCALE(0, 0), SCALE(0, 0), SCALE(elements->window.padding));
+				{
+					if (var->gui.active_stage == 1)
+					{
+						widgets->games_child("fps_id", "FPS", "H", "25.201");
+						{
+							for (int i = 0; i < elements->game_card.fps_games.size(); i++)
+							{
+								widgets->game_card(elements->game_card.fps_games.at(i) + "idfps", elements->game_card.fps_games.at(i), elements->game_card.fps_count.at(i), i, elements->game_card.fps_ready.at(i));
+								if ((i + 1) % 3 != 0)
+									gui->sameline();
+							}
+						}
+						widgets->games_end_child();
+						widgets->games_child("battle_royale_id", gui->language("BATTLE ROYALE", "КОРОЛЕВСКАЯ БИТВА"), "I", "20.186");
+						{
+							for (int i = 0; i < elements->game_card.royale_games.size(); i++)
+							{
+								widgets->game_card(elements->game_card.royale_games.at(i) + "idroyale", elements->game_card.royale_games.at(i), elements->game_card.royale_count.at(i), i + 3, elements->game_card.royale_ready.at(i));
+								if ((i + 1) % 3 != 0)
+									gui->sameline();
+							}
+						}
+						widgets->games_end_child();
+					}
+					if (var->gui.active_stage == 2)
+						widgets->product_page(var->gui.cs_player, 0, "Counter-Strike 2", gui->language("Step into the intense, tactical world of Counter-Strike 2 armed with an unrivaled advantage. Our cheat is meticulously crafted for the Source 2 engine, offering a seamless and powerful experience. Gain complete tactical awareness with our crystal-clear ESP, revealing enemy positions, health, and equipment through any obstacle. Beyond core combat, enhance your gameplay with a full-featured Skin Changer to equip any cosmetic you desire.", "Войдите в напряженный, тактический мир Counter-Strike 2, вооружившись непревзойденным преимуществом. Наш чит тщательно разработан для движка Source 2, обеспечивая бесперебойную и мощную работу. Получите полную тактическую осведомленность благодаря нашему кристально чистому ESP, позволяющему видеть позиции врагов, их здоровье и снаряжение сквозь любые препятствия. Помимо основного боя, вы сможете улучшить свой игровой процесс с помощью полнофункционального скин-чейнджера, позволяющего надеть любую косметику по вашему желанию."), "12.679", "22.07.25", "Undetected", "146");
+					if (var->gui.active_stage == 4)
+						widgets->product_page(var->gui.apex_player, 4, "Apex Legends", gui->language("Step into the intense, tactical world of Counter-Strike 2 armed with an unrivaled advantage. Our cheat is meticulously crafted for the Source 2 engine, offering a seamless and powerful experience. Gain complete tactical awareness with our crystal-clear ESP, revealing enemy positions, health, and equipment through any obstacle. Beyond core combat, enhance your gameplay with a full-featured Skin Changer to equip any cosmetic you desire.", "Войдите в напряженный, тактический мир Counter-Strike 2, вооружившись непревзойденным преимуществом. Наш чит тщательно разработан для движка Source 2, обеспечивая бесперебойную и мощную работу. Получите полную тактическую осведомленность благодаря нашему кристально чистому ESP, позволяющему видеть позиции врагов, их здоровье и снаряжение сквозь любые препятствия. Помимо основного боя, вы сможете улучшить свой игровой процесс с помощью полнофункционального скин-чейнджера, позволяющего надеть любую косметику по вашему желанию."), "12.679", "22.07.25", "Undetected", "146");
+					if (var->gui.active_stage == 5)
+						widgets->product_page(var->gui.fortnite_player, 2, "Fortnite", gui->language("Step into the intense, tactical world of Counter-Strike 2 armed with an unrivaled advantage. Our cheat is meticulously crafted for the Source 2 engine, offering a seamless and powerful experience. Gain complete tactical awareness with our crystal-clear ESP, revealing enemy positions, health, and equipment through any obstacle. Beyond core combat, enhance your gameplay with a full-featured Skin Changer to equip any cosmetic you desire.", "Войдите в напряженный, тактический мир Counter-Strike 2, вооружившись непревзойденным преимуществом. Наш чит тщательно разработан для движка Source 2, обеспечивая бесперебойную и мощную работу. Получите полную тактическую осведомленность благодаря нашему кристально чистому ESP, позволяющему видеть позиции врагов, их здоровье и снаряжение сквозь любые препятствия. Помимо основного боя, вы сможете улучшить свой игровой процесс с помощью полнофункционального скин-чейнджера, позволяющего надеть любую косметику по вашему желанию."), "12.679", "22.07.25", "Undetected", "146");
+
+				}
+				gui->end_content();
+			}
+			gui->pop_var();
+		}
+		gui->end_content();
+		gui->pop_var();
+
+		bool bg{ false };
+		if (gui->is_window_cond(GetCurrentContext()->NavWindow, { "dropdown" }))
+			bg = true;
+		gui->easing(elements->loading.window_alpha, bg || var->gui.loading ? 1.f : 0.f, 8.f, static_easing);
+
+		gui->set_pos(SCALE(0, 0), pos_all);
+		gui->push_var(style_var_alpha, elements->loading.window_alpha);
+		gui->begin_content("back_alpha", SCALE(0, 0), SCALE(0, 0), SCALE(0, 0));
+		{
+			draw->rect_filled(gui->window_drawlist(), gui->window_pos(), gui->window_pos() + gui->window_size(), draw->get_clr(clr->window.background, 0.6), SCALE(var->window.rounding));
+			gui->loading();
+		}
+		gui->end_content();
+		gui->pop_var();
+
+		gui->move_window(var->winapi.hwnd, var->winapi.rc);
+	}
+	gui->end();
+
+	if (ImGui::IsKeyPressed(ImGuiKey_Equal) && ImGui::IsKeyDown(ImGuiKey_LeftAlt) && var->gui.stored_dpi < 200)
+	{
+		var->gui.stored_dpi += 10;
+		var->gui.dpi_changed = true;
+	}
+
+	if (ImGui::IsKeyPressed(ImGuiKey_Minus) && ImGui::IsKeyDown(ImGuiKey_LeftAlt) && var->gui.stored_dpi > 80)
+	{
+		var->gui.stored_dpi -= 10;
+		var->gui.dpi_changed = true;
+	}
+
+	if (var->gui.dpi != var->gui.stored_dpi / 100.f)
+	{
+		var->gui.dpi_changed = true;
+		var->gui.update_size = true;
+	}
+}
---
+++ zS1Tv5D/UIEngine/framework/widgets/pages.cpp
@@ -0,0 +1,118 @@
+﻿#include"../headers/includes.h"
+#include "../headers/widgets.h"
+
+void c_widgets::log_reg_page()
+{
+	gui->begin_content("log_reg_zone", SCALE(elements->log_reg_page.log_reg_width, 0), SCALE(elements->log_reg_page.padding), SCALE(0, elements->window.padding.y), window_flags_no_scroll_with_mouse | window_flags_no_scrollbar);
+	{
+		draw->shadow_circle(gui->window_drawlist(), gui->window_pos() + ImVec2(gui->window_size().x / 2, SCALE(elements->log_reg_page.padding.y + elements->log_reg_page.shadow_radius)), SCALE(elements->log_reg_page.shadow_radius), draw->get_clr(clr->main.accent, 0.1), SCALE(100), ImVec2(0, 0), 0, 60);
+		draw->text_clipped(gui->window_drawlist(), font->get(icons_data, 40), gui->window_pos() + SCALE(0, elements->log_reg_page.padding.y + 4), gui->window_pos() + gui->window_size(), draw->get_clr(clr->main.accent), "A", NULL, NULL, ImVec2(0.5f, 0.f));
+		draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_medium_data, 16), gui->window_pos() + SCALE(0, elements->log_reg_page.padding.y + elements->window.padding.y + elements->log_reg_page.shadow_radius * 2 - 4), gui->window_pos() + gui->window_size(), draw->get_clr(clr->main.text), "UIEngine", NULL, NULL, ImVec2(0.5f, 0.f));
+		draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_regular_data, 14), gui->window_pos() + ImVec2(0, SCALE(elements->log_reg_page.padding.y + elements->window.padding.y + elements->log_reg_page.shadow_radius * 2 + 1) + gui->text_size(font->get(suisse_intl_medium_data, 16), "UIEngine").y), gui->window_pos() + gui->window_size(), draw->get_clr(clr->main.text, 0.48), "Play with confidence.", NULL, NULL, ImVec2(0.5f, 0.f));
+		
+		gui->easing(elements->log_reg_page.window_height, var->gui.registration ? (elements->textfield.size.y * 4 + elements->window.padding.y + elements->widgets.spacing.y * 2) : (elements->textfield.size.y * 3 + elements->window.padding.y + elements->widgets.spacing.y * 1), 12.f, dynamic_easing);
+		gui->set_pos(ImVec2((gui->window_width() - SCALE(elements->textfield.size.x)) / 2, (gui->window_height() - SCALE(elements->log_reg_page.window_height)) / 2), pos_all);
+		gui->begin_content("registration_zone", SCALE(elements->textfield.size.x, 0), SCALE(0, 0), SCALE(0, elements->window.padding.y), window_flags_no_scroll_with_mouse | window_flags_no_scrollbar);
+		{
+			gui->begin_content("fields_zone", SCALE(elements->textfield.size.x, elements->log_reg_page.window_height - (elements->textfield.size.y + elements->window.padding.y)), SCALE(0, 0), SCALE(0, elements->widgets.spacing.y), window_flags_no_scroll_with_mouse | window_flags_no_scrollbar);
+			{
+				static char email[20];
+				if(var->gui.registration)
+					widgets->text_field("email_field", "Email", "d", email, sizeof(email));
+				static char username[20];
+				widgets->text_field("username_field", "Username", "B", username, sizeof(username));
+				static char password[20];
+				widgets->text_field("password_field", "Password", "C", password, sizeof(password));
+
+				if (!var->gui.registration && (var->gui.username == username && var->gui.password == password) && var->gui.registered)
+					var->gui.stage_count = 1;
+				if (var->gui.registration && email[0] != '\0' &&   username[0] != '\0' && password[0] != '\0' && var->gui.registered)
+					var->gui.stage_count = 1;
+			}
+			gui->end_content();
+
+			widgets->reg_log_buttons();
+		}
+		gui->end_content();
+		gui->set_pos(ImVec2((gui->window_width() - SCALE(elements->textfield.size.x)) / 2, gui->window_height() - SCALE(elements->log_reg_page.padding.y + elements->version_card.rect_size.y * elements->version_card.games.size() + elements->version_card.padding * (elements->version_card.games.size() - 1) + elements->version_card.zone_padding + elements->version_card.text_zone_height)), pos_all);
+		gui->begin_content("versions_zone", SCALE(elements->textfield.size.x, elements->log_reg_page.padding.y + elements->version_card.rect_size.y * elements->version_card.games.size() + elements->version_card.padding * (elements->version_card.games.size() - 1) + elements->version_card.zone_padding + elements->version_card.text_zone_height), SCALE(0, 0), SCALE(0, elements->version_card.padding));
+		{
+			draw->text_clipped(gui->window_drawlist(), font->get(icons_data, 10), gui->window_pos(), gui->window_pos() + SCALE(elements->version_card.rect_size.x, elements->version_card.text_zone_height), draw->get_clr(clr->main.text, 0.24), "W", NULL, NULL, ImVec2(0.5f, 0.5f));
+			draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_semi_bold_data, 13), gui->window_pos() + SCALE(elements->version_card.rect_size.x + elements->version_card.zone_padding, 0), gui->window_pos() + ImVec2(gui->window_width(), SCALE(elements->version_card.text_zone_height)), draw->get_clr(clr->main.text, 0.48), "VERSION 1.2", NULL, NULL, ImVec2(0.f, 0.5f));
+			draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_semi_bold_data, 13), gui->window_pos(), gui->window_pos() + ImVec2(gui->window_width(), SCALE(elements->version_card.text_zone_height)), draw->get_clr(clr->main.text, 0.12), "22.07.25", NULL, NULL, ImVec2(1.f, 0.5f));
+
+			gui->set_pos(SCALE(elements->version_card.text_zone_height + elements->version_card.zone_padding), pos_y);
+			for (int i = 0; i < elements->version_card.games.size(); i++)
+				widgets->version_card(elements->version_card.games.at(i) + "id", elements->version_card.games.at(i), elements->version_card.updates.at(i), i);
+		}
+		gui->end_content();
+	}
+	gui->end_content();
+	gui->sameline();
+	gui->begin_content("decoration_zone", ImVec2(0, 0), SCALE(0, 0), SCALE(elements->widgets.spacing.x, elements->window.padding.y), window_flags_no_scroll_with_mouse | window_flags_no_scrollbar);
+	{
+		draw->image_rounded(gui->window_drawlist(), var->gui.decoration[0], gui->window_pos(), gui->window_pos() + ImVec2(gui->window_size().x, SCALE(elements->log_reg_page.img_height)), ImVec2(0, 0), ImVec2(1, 1), draw->get_clr({1.f, 1.f, 1.f, 1.f}), SCALE(elements->widgets.rounding));
+		draw->rect(gui->window_drawlist(), gui->window_pos(), gui->window_pos() + ImVec2(gui->window_size().x, SCALE(elements->log_reg_page.img_height)), draw->get_clr(clr->main.text, 0.08), SCALE(elements->widgets.rounding));
+		gui->set_pos(SCALE(0, elements->window.padding.y + elements->log_reg_page.img_height), pos_all);
+
+		widgets->info_card("launches_1_id", "E", "Launches", "45.387", (gui->window_size().x - SCALE(elements->widgets.spacing.x * 2)) / 3);
+		gui->sameline();
+		widgets->info_card("total_users_1_id", "a", "Total Users", "16.274", (gui->window_size().x - SCALE(elements->widgets.spacing.x * 2)) / 3);
+		gui->sameline();
+		widgets->info_card("products_1_id", "G", "Products", "6", (gui->window_size().x - SCALE(elements->widgets.spacing.x * 2)) / 3);
+	}
+	gui->end_content();
+};
+
+void c_widgets::product_page(c_video_player& player, int img_id, std::string_view name, std::string_view desc, std::string_view launches, std::string_view updated, std::string_view status, std::string_view online)
+{
+	gui->begin_content("product_desc_content", ImVec2(gui->content_max().x - SCALE(elements->player.size.x + elements->window.padding.x), gui->content_avail().y), SCALE(0, 0), SCALE(elements->widgets.spacing), window_flags_no_scrollbar | window_flags_no_scroll_with_mouse);
+	{
+		ImGuiContext& g = *GImGui;
+		widgets->back_button();
+
+		const ImRect game_rect(ImVec2(g.LastItemData.Rect.Min.x, g.LastItemData.Rect.Max.y + SCALE(elements->widgets.spacing.y)), ImVec2(g.LastItemData.Rect.Min.x + gui->content_avail().x, g.LastItemData.Rect.Max.y + SCALE(elements->widgets.spacing.y + elements->product_page.game_zone_height)));
+		const ImRect desc_rect(ImVec2(game_rect.Min.x, game_rect.Max.y + SCALE(elements->product_page.back_button_padding)), gui->window_pos() + gui->window_size() - SCALE(0, elements->widgets.spacing.y * 2 + elements->info_card.height * 2));
+
+		draw->image_rounded(gui->window_drawlist(), var->gui.img_for_versions[img_id], ImVec2(game_rect.Min.x, game_rect.GetCenter().y - SCALE(elements->product_page.img_size.y / 2)), ImVec2(game_rect.Min.x + SCALE(elements->product_page.img_size.x), game_rect.GetCenter().y + SCALE(elements->product_page.img_size.y / 2)), ImVec2(0, 0), ImVec2(1, 1), draw->get_clr({1.f, 1.f, 1.f, 1.f}), SCALE(elements->version_card.rounding));
+	
+		draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_medium_data, 19), game_rect.Min + SCALE(elements->product_page.img_size.x + elements->product_page.back_button_padding, 0), game_rect.Max, draw->get_clr(clr->main.text), name.data(), gui->text_end(name.data()), NULL, ImVec2(0.f, 0.5f));
+	
+		std::vector<std::string> lines = gui->wrap_text(font->get(suisse_intl_medium_data, 13), gui->content_avail().x, std::string(desc));
+
+		ImVec2 desc_pos = desc_rect.Min;
+		for (const auto& line : lines)
+		{
+			draw->text_clipped(gui->window_drawlist(), font->get(suisse_intl_medium_data, 13), desc_pos, desc_rect.Max, draw->get_clr(clr->main.text, 0.48), line.c_str());
+			desc_pos.y += gui->text_size(font->get(suisse_intl_medium_data, 13), "A").y;
+		}
+
+		gui->set_screen_pos(ImVec2(desc_rect.Min.x, desc_rect.Max.y + SCALE(elements->widgets.spacing.y)), pos_all);
+		gui->begin_group();
+		{
+			widgets->info_card("launches_2_id", "E", gui->language("Launches", "Запущено"), "12.679", (gui->window_size().x - SCALE(elements->widgets.spacing.x)) / 2);
+			gui->sameline();
+			widgets->info_card("updated_2_id", "W", gui->language("Updated", "Обновлено"), "22.07.25", (gui->window_size().x - SCALE(elements->widgets.spacing.x)) / 2);
+		}
+		gui->end_group();
+		gui->begin_group();
+		{
+			widgets->info_card("status_2_id", "b", gui->language("Status", "Статус"), gui->language("Undetected", "Незамечен"), (gui->window_size().x - SCALE(elements->widgets.spacing.x)) / 2);
+			gui->sameline();
+			widgets->info_card("online_2_id", "a", gui->language("Online", "Онлайн"), "146", (gui->window_size().x - SCALE(elements->widgets.spacing.x)) / 2);
+		}
+		gui->end_group();
+	}
+	gui->end_content();
+	gui->sameline();
+	gui->begin_content("player_content", ImVec2(SCALE(elements->player.size.x), gui->content_avail().y), SCALE(0, 0), SCALE(elements->widgets.spacing), window_flags_no_scrollbar | window_flags_no_scroll_with_mouse);
+	{
+		player.render(std::string("cs_demo"), SCALE(var->window.size), name);
+		gui->set_screen_pos(GImGui->LastItemData.Rect.Max.y + SCALE(elements->window.padding.y), pos_y);
+		if (widgets->launch_button())
+			var->gui.loading = true;
+		widgets->update_button();
+	}
+	gui->end_content();
+
+};
